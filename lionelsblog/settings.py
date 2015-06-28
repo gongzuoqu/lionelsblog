e641c0a8147a1041f97c08562ac394542d488a63
Lionel Brahami <lionel.brahami@gmail.com>
2015-06-28 18:20:27 +0800

    change templates path

--- lionelsblog/settings.py
+++ lionelsblog/settings.py
@@ -104,7 +104,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#TEMPLATE_DIRS = (os.path.join(BASE_DIR, "templates"), )
+TEMPLATE_DIRS = (os.path.join(BASE_DIR, "blog", "templates"), )
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "blog", "static"), )
 
 STATIC_ROOT = "/var/www/django/lionelsblog/static/"

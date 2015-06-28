85d752cdf3d63e8945efe2485d011aab3d0b4cbc
Lionel Brahami <lionel.brahami@gmail.com>
2015-06-28 16:44:33 +0800

    move static and templates to blog app

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,10 @@ from . import models
 
 class BlogIndex(generic.ListView):
     queryset = models.Entry.objects.published()
-    template_name = "home.html"
+    template_name = "blog/home.html"
     paginate_by = 2
 
 
 class BlogDetail(generic.DetailView):
     model = models.Entry
-    template_name = "post.html"
+    template_name = "blog/post.html"
